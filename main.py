be423931de8b158818974fd62162419dbec29280
sophianyap <adobongmanok04@outlook.com>
2024-11-09 05:32:53 +0800

    final-changes-and-bgm

--- main.py
+++ main.py
@@ -60,6 +60,7 @@ class UIElement(Sprite):
         surface.blit(self.image, self.rect)
 
 def confirm_quit(screen):
+
     background = pygame.image.load("New folder/exitquestion.png")
 
     BLACK = (0, 0, 0)
@@ -103,6 +104,12 @@ def confirm_quit(screen):
         pygame.display.flip()
 
 def title_screen(screen):
+
+    pygame.mixer.init()
+    pygame.mixer.music.load("DavidKBD - Spooky Pack - Spooky Playtime-03 - Haunted Mansion.ogg")  # Replace with your actual music file path
+    pygame.mixer.music.set_volume(0.5)  # Optional: Set volume to 50%
+    pygame.mixer.music.play(-1)
+
     title1_font = pygame.font.SysFont("Inter", 34, bold=True)
     title1_text = title1_font.render("THE RISE OF THE", True, WHITE)
     title1_rect = title1_text.get_rect(center=(400, 200))
@@ -157,8 +164,15 @@ def title_screen(screen):
 
         pygame.display.flip()
         clock.tick(30)
+        
 
 def play_level(screen, database: Database):
+
+    pygame.mixer.init()
+    pygame.mixer.music.load("mainbg.mp3")  # Replace with your actual music file path
+    pygame.mixer.music.set_volume(0.5)  # Optional: Set volume to 50%
+    pygame.mixer.music.play(-1)
+
     return_btn = UIElement((680, 60), "Main Menu", 30, None, WHITE, action=GameState.TITLE)
     background = pygame.image.load("New folder/mainbackground.png")
 
@@ -189,9 +203,14 @@ def play_level(screen, database: Database):
         clock.tick(60)
 
 def loading_screen(screen):
-    background = pygame.image.load("New folder/loadingbg.png")
+
+    pygame.mixer.init()
+    pygame.mixer.music.load("Thunderstorm sound effect.mp3")  # Replace with your actual music file path
+    pygame.mixer.music.set_volume(0.5)  # Optional: Set volume to 50%
+    pygame.mixer.music.play(-1)
     animation = [pygame.image.load(f"New folder/load ({i}).png") for i in range(1, 4)]
-    screen.blit(background, (0, 0))
+    animationbg = [pygame.image.load(f"New folder/loadingbg/{i}.png") for i in range(16, 23)]
+    
     start_time = time.time()
     clock = pygame.time.Clock()
 
@@ -217,18 +236,18 @@ def loading_screen(screen):
         delay_counter += 1
         if delay_counter >= frame_delay:
             frame = (frame + 1) % len(animation)
-            delay_counter = 6
-        if elapsed_time >= 5:
+            delay_counter = 1
+        if elapsed_time >= 7:
             return
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return GameState.QUIT
             if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
                 mouse_up = True
-
-        screen.blit(background, (0, 0))
-        screen.blit(title1_text, title1_rect)
+        
+        screen.blit(animationbg[frame], (0, 0))
         screen.blit(animation[frame], (0, 0))
+        screen.blit(title1_text, title1_rect)
         pygame.display.flip()
         clock.tick(30)
 
@@ -303,43 +322,77 @@ def leaderboard(screen, database: Database):
 
         pygame.display.flip()
 
-
-import pygame
-import sys
-
 def story_screen(screen, player_name):
+
+    pygame.mixer.init()
+    pygame.mixer.music.load("mainbg.mp3")  # Replace with your actual music file path
+    pygame.mixer.music.set_volume(0.5)  # Optional: Set volume to 50%
+    pygame.mixer.music.play(-1)
+
 
     background = pygame.image.load("New folder/storytime.png")
     screen.blit(background, (0, 0))
+    animation = [pygame.image.load(f"New folder/chester ({i}).png") for i in range(1, 5)]
 
     game_state = GameState.STORY
     title1_font = pygame.font.Font(fount, 60)  
     tutorial_font = pygame.font.Font(fount, 30)
+
+    title1_font = pygame.font.Font(fount,60)  
+    title1_text = title1_font.render("WELCOME, GENIUS! ", True, WHITE)
+    title1_rect = title1_text.get_rect(center=(400,150))
+
+    title2_font = pygame.font.Font(fount,20) 
+    title2_text = title2_font.render("press anywhere to continue...", True, WHITE)
+    title2_rect = title2_text.get_rect(center=(400,550))
     
     tutorial_text_lines = [
-        "Welcome to the game!",
+        
         "Use the arrow keys to move.",
         "Press space to interact with objects.",
         "Avoid enemies and collect power-ups.",
         "Good luck, and have fun!"
     ]
 
+    start_time = time.time()
+    clock = pygame.time.Clock()
+
+    frame = 0
+    frame_delay = 10
+    delay_counter = 0
+    clock = pygame.time.Clock()
+
+
     tutorial_text = []
     for line in tutorial_text_lines:
         tutorial_text.append(tutorial_font.render(line, True, WHITE))
-
+    final_line_font = pygame.font.Font(fount, 40)  
     final_line = f"Are you ready, {player_name}?"
-    final_text = title1_font.render(final_line, True, WHITE)
+    final_text = final_line_font.render(final_line, True,  RED)
 
     tutorial_rects = []
     for i, line in enumerate(tutorial_text):
-        rect = line.get_rect(center=(400, 150 + (i * 40)))
+        rect = line.get_rect(center=(400, 250 + (i * 40)))
         tutorial_rects.append(rect)
 
-    final_text_rect = final_text.get_rect(center=(400, 150 + len(tutorial_text) * 40 + 60))
+    final_text_rect = final_text.get_rect(center=(400, 200 + len(tutorial_text) * 40 + 60))
 
     while True:
         mouse_up = False
+        elapsed_time = time.time() - start_time
+
+        delay_counter += 1
+        if delay_counter >= frame_delay:
+            frame = (frame + 1) % len(animation)
+            delay_counter = 1
+        if elapsed_time >= 7:
+            return
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return GameState.QUIT
+            if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                mouse_up = True
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -348,23 +401,60 @@ def story_screen(screen, player_name):
                 mouse_up = True  
 
         screen.blit(background, (0, 0))
+        screen.blit(title1_text,title1_rect)
+        screen.blit(title2_text,title2_rect)
+        screen.blit(animation[frame], (0, 0))
         
         for i, line in enumerate(tutorial_text):
             screen.blit(line, tutorial_rects[i])
 
-        screen.blit(final_text, final_text_rect)
-
+        screen.blit(final_text, final_text_rect)    
         pygame.display.flip()
 
         if mouse_up:
-            return GameState.TITLE  # Or any other state you want to transition to
+            return GameState.TITLE 
 
 def get_player_name_from_database():
 
-    return "John Doe"  
-
+    return "John Doe" 
 #
 player_name = get_player_name_from_database()
+
+def finish_level(screen):
+
+    background = pygame.image.load("New folder/gameover.png")
+    title1_font = pygame.font.Font(fount, 90) 
+    title1_text = title1_font.render("YOU WIN", True, WHITE)
+    title1_rect = title1_text.get_rect(center=(400, 300))
+
+    start_btn = UIElement((400, 400), "RETRY", 60, None, WHITE, action=GameState.NEWGAME)
+    return_btn = UIElement((110, 70), "MAIN MENU", 30, None, WHITE, action=GameState.TITLE)
+    quit_btn = UIElement((690, 70), "QUIT GAME", 30, None, WHITE, action=GameState.QUIT)
+    leaderboard_btn = UIElement((400, 450), "LEADERBOARD", 30, None, WHITE, action=GameState.LEADERBOARD)
+    buttons = [start_btn, quit_btn, leaderboard_btn,return_btn]
+
+
+    while True:
+        mouse_up = False  # Reset mouse_up at the beginning of each frame
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                if confirm_quit(screen):
+                    pygame.quit()
+                    sys.exit()
+            if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                mouse_up = True
+
+        screen.blit(background, (0, 0))
+        screen.blit(title1_text, title1_rect)
+
+        for button in buttons:
+            ui_action = button.update(pygame.mouse.get_pos(), mouse_up)
+            if ui_action:
+                return ui_action
+            button.draw(screen)
+
+        pygame.display.flip()
+
 
 
 def game_over(screen):
@@ -402,10 +492,6 @@ def game_over(screen):
         pygame.display.flip()
 
 
-
-def finish_level():
-    pass
-
 def player_name_screen(screen, database: Database):
     global player_name
 
@@ -424,6 +510,11 @@ def player_name_screen(screen, database: Database):
     color = color_inactive
     active = False
     text = ''
+
+    # Error message font and settings
+    error_font = pygame.font.Font(fount, 28)
+    error_message = ''
+    error_message_rect = None
 
     clock = pygame.time.Clock()
 
@@ -444,27 +535,33 @@ def player_name_screen(screen, database: Database):
             if event.type == pygame.KEYDOWN:
                 if active:
                     if event.key == pygame.K_RETURN:
-                        # Save the player's name to the database and return to title screen
-                        player_name = text
-                        database.save_player(player_name, kills=0, time_spent=0)
-                        return GameState.TITLE
-                    elif event.key == pygame.K_BACKSPACE:
-                        text = text[:-1]  # Remove the last character
-                    else:
-                        text += event.unicode  # Add the typed character
 
-        screen.blit(background, (0, 0))
+                        if not text.strip():
+                            error_message = "Invalid name"
+                            error_message_rect = error_font.render(error_message, True, (255, 0, 0)).get_rect(center=(400, 370))
+                        else:
+                            player_name = text
+                            database.save_player(player_name, kills=0, time_spent=0)
+                            return GameState.TITLE
+                    elif event.key == pygame.K_BACKSPACE:
+                        text = text[:-1]  
+                    else:
+                        text += event.unicode  
+
+        screen.blit(background, (0, 20))
         screen.blit(title_text, title_rect)
 
-        # Render the current text
         txt_surface = input_font.render(text, True, color)
-        width = max(200, txt_surface.get_width() + 10)
-        input_box.w = width
         screen.blit(txt_surface, (input_box.x + 5, input_box.y + 5))
         pygame.draw.rect(screen, color, input_box, 2)
 
+        if error_message:
+            error_text_surface = error_font.render(error_message, True, (255, 0, 0))
+            screen.blit(error_text_surface, error_message_rect)
+
         pygame.display.flip()
         clock.tick(30)
+
 
 
 def main():
@@ -485,7 +582,7 @@ def main():
                 if game_state != GameState.QUIT:
                     game_state = play_level(screen, database)
         elif game_state == GameState.FINISH:
-            game_state = finish_level()
+            game_state = finish_level(screen)
         elif game_state == GameState.QUIT: 
             pygame.quit()
             sys.exit()
